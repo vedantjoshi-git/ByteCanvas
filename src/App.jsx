2c90995573f26ec073e197fbf839f94f0b1829a1
Vedant <vedantjoshi122002@gmail.com>
2023-04-20 01:12:08 +0530

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
+import Banner from './components/Header'
 
 const App = () => {
   return (
-    <div className='min-h-screen text-gray-900 dark:text-gray-300 dark:bg=[#212936]'><Header /></div>
+    <div className='min-h-screen text-gray-900 dark:text-gray-300 dark:bg=[#212936]'><Header /> <Banner/> <Proposals /></div>
   )
 }
 
